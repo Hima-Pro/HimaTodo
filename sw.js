6c8ffadb80e2051fb5da3457fcc16833dd9322a3
Hima-Pro <456293802@fayoum3.moe.edu.eg>
2023-04-22 14:24:05 +0200

    bug fixes

--- sw.js
+++ sw.js
@@ -4,28 +4,15 @@ var fileList = [
   "./assets/css/small.css",
   "./assets/css/style.css",
   "./assets/images/empty.png",
-  "./assets/images/logo-192.png",
-  "./assets/images/logo-512.png",
   "./assets/images/logo.svg",
-  "./assets/images/maskable-192.png",
-  "./assets/images/maskable-512.png",
-  "./assets/images/screenshots/main-large-dark.png",
-  "./assets/images/screenshots/main-large-light.png",
-  "./assets/images/screenshots/main-small-dark.png",
-  "./assets/images/screenshots/tags-larg-dark.png",
   "./assets/js/helpers.js",
   "./assets/js/idb.js",
   "./assets/js/script.js",
-  "./assets/lsf",
   "./assets/manifest.json",
   "./favicon.ico",
-  "./files.json",
   "./index.html",
-  "./LICENSE",
-  "./README.md",
   "./sw.js"
-]
-;
+];
 
 self.addEventListener("install", (e) => {
   e.waitUntil((async () => {
